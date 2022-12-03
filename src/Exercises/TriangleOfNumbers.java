7c2b125460b1a5b89790da587f212a47b55acde8
Bobi <bobi.bg@gmail.com>
2022-12-03 10:57:10 +0200

    Java Fundamentals

--- src/Exercises/TriangleOfNumbers.java
+++ src/Exercises/TriangleOfNumbers.java
@@ -8,5 +8,18 @@ public class TriangleOfNumbers {
         Scanner scan = new Scanner(System.in);
 
         int n = Integer.parseInt(scan.nextLine());
+
+        for (int i = 1; i <= n; i++) {
+
+
+            for (int j = 1; j <= i; j++) {
+
+                System.out.print(i+ " ");
+
+            }
+
+            System.out.println();
+
+        }
     }
 }
