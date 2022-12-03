cefe1548c9f812e4c95e4dd86869346bfcf537b9
Bobi <bobi.bg@gmail.com>
2022-12-03 10:19:23 +0200

    Java Fundamentals

---
+++ src/Exercises/TriangleOfNumbers.java
@@ -0,0 +1,12 @@
+package Exercises;
+
+import java.util.Scanner;
+
+public class TriangleOfNumbers {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int n = Integer.parseInt(scan.nextLine());
+    }
+}
