0ff0799923e32cbd59dabcfa42e644906362c44c
Bobi <bobi.bg@gmail.com>
2022-12-10 16:40:41 +0200

    Java Fundamentals

--- src/Lab/SpecialNumbers.java
+++ src/Lab/SpecialNumbers.java
@@ -1,7 +1,7 @@
 package Lab;
 
 import java.util.Scanner;
-public class specialnumbers {
+public class SpecialNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
