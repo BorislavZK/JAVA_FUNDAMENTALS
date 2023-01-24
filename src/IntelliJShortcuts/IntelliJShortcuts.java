cd323e1332462b9d5ccab5f82a043b4662e49fdf
Bobi <bobi.bg@gmail.com>
2023-01-24 08:38:31 +0200

    Java Fundamentals 23:01:2023

---
+++ src/IntelliJShortcuts/IntelliJShortcuts.java
@@ -0,0 +1,20 @@
+package IntelliJShortcuts;
+
+public class IntelliJShortcuts {
+    public static void main(String[] args) {
+
+
+        // Jump to words
+        // Alt + ->
+        // Alt + <-
+
+        // Select word
+        // Alt + DownArrow
+
+        // Select paragraph
+        // Shift + ->
+        // Shift + <-
+
+
+    }
+}
---
+++ src/Lists/MergingLists.java
@@ -0,0 +1,11 @@
+package Lists;
+
+import java.util.Scanner;
+
+public class MergingLists {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
