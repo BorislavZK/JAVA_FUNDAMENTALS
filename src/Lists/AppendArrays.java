0001b728695797b8b3cf7dc062948a29b5d71e13
Bobi <bobi.bg@gmail.com>
2023-02-06 22:29:20 +0200

    Java Fundamentals 06:02:2023

---
+++ src/Lists/AppendArrays.java
@@ -0,0 +1,18 @@
+package Lists;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class AppendArrays {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> arrays = Arrays.stream(scanner.nextLine().split("\\|")).collect(Collectors.toList());
+        Collections.reverse(arrays);
+        String toPrint = arrays.toString().replaceAll("[\\]\\[,]", "").trim();
+        toPrint = toPrint.replaceAll("\\s+", " ");
+
+        System.out.println(toPrint);
+    }
+}
--- src/Lists/CardGame.java
+++ src/Lists/CardGame.java
@@ -11,39 +11,35 @@ public class CardGame {
         List<Integer> playerOne = Arrays.stream(scan.nextLine().split("\\s+"))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
+
         List<Integer> playerTwo = Arrays.stream(scan.nextLine().split("\\s+"))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
-        int firstSum = 0;
-        int secondSum = 0;
+
+
         while (playerOne.size() > 0 && playerTwo.size() > 0){
 
-            int firstPlayerCard = playerOne.get(0);
-            int secondPlayerCard = playerTwo.get(0);
-
-            if()
-
-            /*
-            if (firstPlayerCard == secondPlayerCard){
-                playerOne.remove(0);
-                playerTwo.remove(0);
-
-            }else if (playerOne.get(0) > playerTwo.get(0)){
+            if(playerOne.get(0) > playerTwo.get(0)){
                 playerOne.add(playerOne.get(0));
                 playerOne.add(playerTwo.get(0));
                 playerOne.remove(0);
                 playerTwo.remove(0);
-            } else {
-                playerTwo.add(playerOne.get(0));
+            }else if(playerOne.get(0) < playerTwo.get(0)){
                 playerTwo.add(playerTwo.get(0));
+                playerTwo.add(playerOne.get(0));
                 playerTwo.remove(0);
                 playerOne.remove(0);
-
+            }else {
+                playerOne.remove(0);
+                playerTwo.remove(0);
             }
-             */
+
         }
 
-        if(playerTwo.isEmpty()){
+        int firstSum = 0;
+        int secondSum = 0;
+
+        if(playerTwo.size() == 0){
             for (Integer poSum : playerOne) {
                 firstSum += poSum;
 
@@ -52,10 +48,10 @@ public class CardGame {
         } else {
             for (Integer ptSum : playerTwo) {
                 secondSum += ptSum;
+
             }
 
             System.out.printf("Second player wins! Sum: %d", secondSum);
-
         }
     }
 }
