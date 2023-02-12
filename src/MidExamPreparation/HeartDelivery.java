c405b21d7256048be2177baeac58e3f2ef951a82
Bobi <bobi.bg@gmail.com>
2023-02-12 13:26:25 +0200

    Java Fundamentals 12:02:2023

---
+++ src/MidExamPreparation/HeartDelivery.java
@@ -0,0 +1,11 @@
+package MidExamPreparation;
+
+import java.util.Scanner;
+
+public class HeartDelivery {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String[] input = scan.nextLine().split("@");
+    }
+}
