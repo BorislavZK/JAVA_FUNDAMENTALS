54e02726135bba8691774cad1115c3ff325e088a
Bobi <bobi.bg@gmail.com>
2023-02-12 13:06:11 +0200

    Java Fundamentals 12:02:2023

--- src/MidExamPreparation/ShootForTheWin.java
+++ src/MidExamPreparation/ShootForTheWin.java
@@ -12,23 +12,41 @@ public class ShootForTheWin {
         int[] numbers = new int[input.length];
 
         for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = Integer.parseInt(input[1]);
+            numbers[i] = Integer.parseInt(input[i]);
 
         }
+
+        int totalShots = 0;
 
         String coordinates = scan.nextLine();
-
         while (!coordinates.equals("End")) {
 
+            int valueOfIndex = 0;
             int target = Integer.parseInt(coordinates);
-            if (target >= 0 && target < numbers.length){
 
+            if (target >= 0 && target < numbers.length) {  // ifTargetValid method
+                valueOfIndex = numbers[target];
+                if (numbers[target] != -1) {
+                    numbers[target] = -1;
+                    totalShots++;
+                    for (int i = 0; i < numbers.length; i++) {
+                            //REDUCE
+                        if (numbers[i] > valueOfIndex && numbers[i] != -1) {
+                            numbers[i] -= valueOfIndex;
+                        } else if (numbers[i] <= valueOfIndex && numbers[i] != -1) {
+                            //INCREASE
+                            numbers[i] += valueOfIndex;
+                        }
+                    }
+                }
             }
-
-
-                coordinates = scan.nextLine();
+            coordinates = scan.nextLine();
         }
 
+        System.out.printf("Shot targets %d -> ",totalShots );
 
+        for (int n : numbers) {
+            System.out.print(n + " ");
+        }
     }
 }
