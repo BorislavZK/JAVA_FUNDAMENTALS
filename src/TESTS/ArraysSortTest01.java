d76429fe4444d430a7104450f9c64079c49b874b
Bobi <bobi.bg@gmail.com>
2023-03-04 13:51:21 +0200

    04.03.2022

---
+++ src/TESTS/ArraysSortTest01.java
@@ -0,0 +1,21 @@
+package TESTS;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class ArraysSortTest01 {
+    public static void main(String[] args) {
+
+        Integer[] array = {6,1,8,7,10,0,5,4,3,2,9};
+        System.out.println("Old order");
+        System.out.println(Arrays.toString(array));
+
+        Arrays.sort(array);
+        System.out.println("New order - Ascending");
+        System.out.println(Arrays.toString(array));
+
+        Arrays.sort(array,Collections.reverseOrder());
+        System.out.println("New order - Descending");
+        System.out.println(Arrays.toString(array));
+    }
+}
