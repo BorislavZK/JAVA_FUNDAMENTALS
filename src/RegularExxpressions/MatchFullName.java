5d30522a6e65f1a261fc2d56f779325cfa42507e
Bobi <bobi.bg@gmail.com>
2023-03-21 23:47:54 +0200

    Java Fundamentals 21:03:2023

---
+++ src/RegularExxpressions/MatchFullName.java
@@ -0,0 +1,24 @@
+package RegularExxpressions;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatchFullName {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String input = scan.nextLine();
+
+        Pattern pattern = Pattern.compile("\\b[A-Z][a-z]+ [A-Z][a-z]+\\b");
+
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()){
+
+            String name = matcher.group();
+
+            System.out.print(name + " ");
+        }
+    }
+}
---
+++ src/RegularExxpressions/MatchPhoneNumber.java
@@ -0,0 +1,28 @@
+package RegularExxpressions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatchPhoneNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String input = scan.nextLine();
+
+        Pattern pattern = Pattern.compile("\\+359([ |\\-])2\\1\\d{3}\\1\\d{4}\\b");
+
+        Matcher matcher = pattern.matcher(input);
+
+        List<String> phoneNumList = new ArrayList<>();
+
+        while (matcher.find()){
+            String phoneNumber = matcher.group();
+            phoneNumList.add(phoneNumber);
+        }
+
+        System.out.println(String.join(", ", phoneNumList));
+    }
+}
