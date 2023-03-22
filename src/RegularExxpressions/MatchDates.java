3d4944f497e1cddcba4c757778f6fc716a3de4ee
Bobi <bobi.bg@gmail.com>
2023-03-22 10:10:41 +0200

    Java Fundamentals 21:03:2023

---
+++ src/RegularExxpressions/MatchDates.java
@@ -0,0 +1,28 @@
+package RegularExxpressions;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatchDates {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String input = scan.nextLine();
+
+        //(?<day>\d{2})([-.\\/])(?<month>[A-Z][a-z]{2})(?:\2)(?<year>\d{4})
+
+        Pattern pattern = Pattern.compile("(?<day>\\d{2})([-.\\\\/])(?<month>[A-Z][a-z]{2})(?:\\2)(?<year>\\d{4})");
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()){
+
+            String day = matcher.group("day");
+            String month = matcher.group("month");
+            String year = matcher.group("year");
+
+            System.out.printf("Day: %s, Month: %s, Year: %s\n", day,month,year);
+        }
+    }
+}
