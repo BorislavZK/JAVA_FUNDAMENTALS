10d389e23e7bc99ea06ee92eb9a4b93469633fbf
Bobi <bobi.bg@gmail.com>
2023-03-27 17:01:49 +0300

    Java Fundamentals 27:03:2023

--- src/FinalExamPrep01/EmojiDetector.java
+++ src/FinalExamPrep01/EmojiDetector.java
@@ -1,7 +1,64 @@
 package FinalExamPrep01;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class EmojiDetector {
     public static void main(String[] args) {
+        //([::]{2}|[**]{2})[A-Z][a-z]+\1
 
+        Scanner scan = new Scanner(System.in);
+
+        String input = scan.nextLine();
+
+        long threshold = getThreshold(input);
+
+        Pattern pattern = Pattern.compile("[:][:][A-Z][a-z]{2,}[:][:]|[*][*][A-Z][a-z]{2,}[*][*]");
+        //([::]{2}|[**]{2})(?<emoji>[A-Z][a-z]+)\1
+        Matcher matcher = pattern.matcher(input);
+
+        int emojiCounter = 0;
+
+        List<String> coolEmojis = new ArrayList<>();
+
+        while (matcher.find()){
+            emojiCounter ++;
+
+            String currentEmoji = matcher.group();
+            int currentEmojiSum = 0;
+            for (int i = 0; i < currentEmoji.length(); i++) {
+                char currentChar = currentEmoji.charAt(i);
+                currentEmojiSum += currentChar;
+            }
+
+            if (currentEmojiSum > threshold){
+                coolEmojis.add(matcher.group());
+
+            }
+
+
+        }
+
+        System.out.printf("Cool threshold: %d\n", threshold);
+        System.out.printf("%d emojis found in the text. The cool ones are:\n", emojiCounter);
+
+        for (String coolEmoji : coolEmojis) {
+            System.out.println(coolEmoji);
+        }
+
+
+    }
+
+    private static long getThreshold(String input) {
+        int result = 1;
+        for (int i = 0; i < input.length() ; i++) {
+            if (Character.isDigit(input.charAt(i))){
+                result *= Integer.parseInt(String.valueOf(input.charAt(i)));
+            }
+        }
+        return result;
     }
 }
