35c2c8f4277a82ec3a1a94a710bd5133565bfb5b
Bobi <bobi.bg@gmail.com>
2023-03-31 13:28:16 +0300

    Java Fundamentals 31:03:2023

--- src/FinalExamPrep01/DestinationMapper.java
+++ src/FinalExamPrep01/DestinationMapper.java
@@ -1,14 +1,46 @@
 package FinalExamPrep01;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DestinationMapper {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-
-
         String input = scan.nextLine();
+
+        Matcher matcher = Pattern.compile("([=/])(?<destination>[A-Z][A-Za-z]{2,})\\1").matcher(input);
+        //([=/])[A-Z][A-Za-z]{2,}\\1
+
+
+       List<String> destinationList = new ArrayList<>();
+
+        int totalPoints = 0;
+
+        while (matcher.find()){
+
+            String destination = matcher.group("destination");
+
+            destinationList.add(destination);
+
+
+            totalPoints += destination.length();
+
+        }
+
+
+
+        String delimiter = ", ";
+        String str =  String.join(delimiter, destinationList);
+
+        System.out.println("Destinations: " + str);
+        System.out.printf("Travel Points: %d\n", totalPoints);
+
+
+
 
 
     }
---
+++ src/FinalExamPrep01/PlantDiscovery.java
@@ -0,0 +1,13 @@
+package FinalExamPrep01;
+
+import java.util.Scanner;
+
+public class PlantDiscovery {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+
+        int n = Integer.parseInt(scan.nextLine());
+    }
+}
