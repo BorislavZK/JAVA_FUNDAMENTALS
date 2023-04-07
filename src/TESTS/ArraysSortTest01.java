d0f661cba14cad7da39be9e21f9287de39e40a01
Bobi <bobi.bg@gmail.com>
2023-04-07 17:02:19 +0300

    Java Fundamentals 07:03:2023

--- src/TESTS/ArraysSortTest01.java
+++ src/TESTS/ArraysSortTest01.java
@@ -1,7 +1,9 @@
 package TESTS;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class ArraysSortTest01 {
@@ -20,6 +22,17 @@ public class ArraysSortTest01 {
         System.out.println(Arrays.toString(array));
 
         // studentsList.sort(Comparator.comparingDouble(Students::getGrade).reversed());
+
+
+
+        List<Integer> list1 = new ArrayList<>();
+
+        ArrayList<Integer> list2 = new ArrayList<>();
+
+
+
+
+
 
         
     }
